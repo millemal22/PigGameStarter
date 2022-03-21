f07cd5ac7f4d498dd873ddd8b8c4b880c8ee775c
Maliyah <millemal22@up.edu>
2022-03-21 13:50:45 -0700

    Lab 6 Checkpoint 2

--- app/src/main/java/edu/up/cs301/pig/PigLocalGame.java
+++ app/src/main/java/edu/up/cs301/pig/PigLocalGame.java
@@ -17,11 +17,15 @@ import android.util.Log;
  */
 public class PigLocalGame extends LocalGame {
 
+    private PigGameState state;
+
     /**
      * This ctor creates a new game state
      */
     public PigLocalGame() {
         //TODO  You will implement this constructor
+        this.state = new PigGameState();
+        //int numPlayers = playerNames.length;
     }
 
     /**
@@ -30,7 +34,12 @@ public class PigLocalGame extends LocalGame {
     @Override
     protected boolean canMove(int playerIdx) {
         //TODO  You will implement this method
-        return false;
+        if (state.playerID == playerIdx){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
@@ -41,7 +50,42 @@ public class PigLocalGame extends LocalGame {
     @Override
     protected boolean makeMove(GameAction action) {
         //TODO  You will implement this method
-        return false;
+        if (action instanceof PigHoldAction){
+            if (state.playerID == 0) {
+                state.player0Score += state.runningTotal;
+                state.setRunningTotal(0);
+
+                //change player 0 to player 1
+                state.setPlayerID(1);
+            }
+            else if (state.playerID == 1){
+                state.player1Score += state.runningTotal;
+                state.setRunningTotal(0);
+
+                //change player 1 to player 0
+                state.setPlayerID(0);
+            }
+            return true;
+        }
+        else if (action instanceof PigRollAction){
+            state.setCurrDieVal((int)Math.random()*7);
+
+            //there's a 1% chance that it'll be seven
+            if (state.currDieVal == 7) {
+                state.runningTotal += 6;
+            }
+            else if (state.currDieVal == 1){
+                state.setCurrDieVal(0);
+
+            }
+            else{
+                state.runningTotal += state.currDieVal;
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
     }//makeMove
 
     /**
@@ -50,6 +94,8 @@ public class PigLocalGame extends LocalGame {
     @Override
     protected void sendUpdatedStateTo(GamePlayer p) {
         //TODO  You will implement this method
+        PigGameState copy = new GameState(this.state);
+        p.sendInfo(copy);
     }//sendUpdatedSate
 
     /**
@@ -62,6 +108,17 @@ public class PigLocalGame extends LocalGame {
     @Override
     protected String checkIfGameOver() {
         //TODO  You will implement this method
+
+        String win = "";
+
+        if (state.player0Score >= 50 || state.player1Score >= 50) {
+            if (state.player0Score > state.player1Score) {
+                win = playerNames[0] + " has won! Score: " + state.player0Score;
+            } else if (state.player1Score > state.player0Score) {
+                win = playerNames[1] + " has won! Score: " + state.player1Score;
+            }
+            return win;
+        }
         return null;
     }
 
